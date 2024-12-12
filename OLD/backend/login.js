ba8b129a21f7d4adac86dc04291412f21f6395f3
Nathalie Johst <nathalie.j@gso.schule.koeln>
2024-12-12 13:19:05 +0100

    fix login and overview

---
+++ OLD/backend/login.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('loginForm');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        window.location.href = 'index.html';
+    });
+});
--- OLD/backend/overview.js
+++ OLD/backend/overview.js
@@ -5,20 +5,23 @@ class weekday{
     }
 
     getAllDescriptions(){
-        tasks.forEach(task => {
-            var allDescriptions = [allDescriptions, task.description].join("\n");
+        let allDescriptions;
+        this.tasks.forEach(task => {
+            allDescriptions = [allDescriptions, task.description].filter(Boolean).join("<br>");
         });
         return allDescriptions;
     }
     getAllDepartments(){
-        tasks.forEach(task => {
-            var allDepartments = [allDepartments, task.department].join("\n");
+        let allDepartments;
+        this.tasks.forEach(task => {
+            allDepartments = [allDepartments, task.department].filter(Boolean).join(", ");
         });
         return allDepartments;
     }
     getAllTimes(){
-        tasks.forEach(task => {
-            var allTimes = [allTimes, task.time].join("\n");
+        let allTimes;
+        this.tasks.forEach(task => {
+            allTimes = [allTimes, task.time].filter(Boolean).join(", ");
         });
         return allTimes;
     }
@@ -33,12 +36,12 @@ class task{
 }
 
 const Activities = [new task("Kaffee kochen", "Küche", 2.25), new task("Büro aufräumen", "IT", 5.00)];
-const Thursday = new weekday("Thursday", Activities);
+const Thursday = new weekday("Donnerstag", Activities);
 
-document.getElementById("newDay").onclick = addThursday();
+document.getElementById("newDay").addEventListener("onClick", function(){addThursday()});
 
 function addThursday(){
-    debugger;
+    //debugger;
     var table = document.getElementById("week");
     console.log(Thursday);
 
@@ -46,12 +49,14 @@ function addThursday(){
     var row = table.getElementsByTagName('tbody')[0].insertRow(-1);
 
     // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+    
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
 
     // Add some text to the new cells:
+    
     cell1.innerHTML = Thursday.day;
     cell2.innerHTML = Thursday.getAllDescriptions();
     cell3.innerHTML = Thursday.getAllDepartments();
