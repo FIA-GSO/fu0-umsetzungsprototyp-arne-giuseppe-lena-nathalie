93fb8377f20b5455ceb7b5e5fe167f15058fc238
Nathalie Johst <nathalie.j@gso.schule.koeln>
2024-12-12 13:19:50 +0100

    simplify

--- OLD/backend/login.js
+++ OLD/backend/login.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('loginForm');
+    var form = document.getElementById('loginForm');
     form.addEventListener('submit', function(event) {
         event.preventDefault();
         const username = document.getElementById('username').value;
